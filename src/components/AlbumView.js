45e93ce07f1b24eb9373c088d7f9ab15d8c22de8
Adrian Mitre <mitrehealthcare@gmail.com>
2022-09-16 19:38:53 -0700

    Added: album and artist's tiles and buttons back and home

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -1,25 +1,55 @@
 // These components will be making separate API calls from the app
 // component to serve specific data about a given album
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 
-const styledText = {
-	fontSize: '20px',
-	textAlign: 'center',
-	color: 'blue'
-}
 
 function AlbumView() {
     const { id } = useParams()
     const [ albumData, setAlbumData ] = useState([])
+    const navigate = useNavigate()
 
-    return (
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/song/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setAlbumData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const justSongs = albumData.filter(entry => entry.wrapperType === 'track')
+
+    const renderSongs = justSongs.map((song, i) => {
+        return (
+            <div key={i}>
+                <p>{song.trackName}</p>
+            </div>
+        )
         
-        <div style={styledText}>
-            <h2>The id passed was: {id}</h2>
-            <p>Album Data Goes Here!</p>
+    })
+   
+    const navButtons = () => {
+        return(
+            <div>
+                <button onClick={() => navigate(-1)}>Back</button>
+                |
+                <button onClick={() => navigate('/')}>Home</button>
+            </div>
+        )
+    } 
+    
+    return (
+        <div>
+             {albumData.length > 0 
+            ? <h2>{albumData[0].collectionName}</h2> 
+            : <h2>Loading...</h2>}
+            {navButtons()}
+            {renderSongs}
         </div>
     )
 }
+
 
 export default AlbumView
